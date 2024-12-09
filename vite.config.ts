ec9e098bfd3581f8386e2d8d00997f79dd580571
Alberto Leyva Guerra (ALG) <albertoleyvaguerra@gmail.com>
2024-12-09 01:34:17 +0100

    chore: adds GitHub pages integration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-name-selector/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
